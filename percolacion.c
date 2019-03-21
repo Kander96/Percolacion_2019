a5cd699621d3e603746c73fa3fc922538089ed57
kevin andersson <kandersson@ws22.labo2>
2019-03-21 16:17:53 -0300

    Andan las funciones auxiliares Aleatorio y Poblar

--- percolacion.c
+++ percolacion.c
@@ -3,34 +3,62 @@
 #include <math.h>
 #include <time.h>
 
-float random(int *seed) 
-int poblar(int *red, int dim, float p)
+#define M 2147483697
+#define Q 127773
+#define R 2836
+#define A 16807
+
+float aleatorio(int *seed); 
+int poblar(int *red, int dim, float p);
 
 int main(int argc,char *argv[]){
 	int dim;
 	float p;
-
-	sscanf(argv[1],"%i",&dim);
-	sscanf(argv[2],"%f",&p);
+	dim=3;
+	p=0.5;
 	
-	printf("%d %.1f",dim,p);	
+	sscanf(argv[1],"%i",&dim);
+	sscanf(argv[2],"%f",&p);	
+
+	int *red;
+	red = (int*)malloc(dim*dim*sizeof(int)); 
+
+
+	
+	//printf("%d %.1f",dim,p);	
+
+	poblar(red,dim,p); //cuando le paso una variable a una funcion no pongo el asterisco a los punteros.
 }
 
-int random(int *seed){
-
+float aleatorio(int *seed){
+	int k;
+	float x;
+	k = (*seed)/Q;
+	*seed=A*(*seed-k*Q)-k*R;
+	if (*seed < 0.0)
+		*seed=(*seed)+M;
+	x=(*seed)/(float) M;
+	return x;
 }
 
-int poblar(int *red, int dim, int p){
+int poblar(int *red, int dim, float p){
 	int i;
 
 	srand(time(NULL));
-	int *seed= (float)rand()/(float)rand_max
+	int *seed;
+	seed = (int*)malloc(sizeof(int));
+	*seed=rand();
+	
 
-	for (i=0, i<dim*dim, i=i+1){
-		*(red+i)=0
-		if(random(*seed)<p)
-			*(red+i)=1; 
+	for (i=0; i<dim*dim; i++){
+		*(red+i)=0;
+		if(aleatorio(seed)<p){
+			*(red+i)=1;}
+		printf("%d", *(red+i)); 
 	}
+	printf("\n");
+	return 0;
 }
+
 
 
