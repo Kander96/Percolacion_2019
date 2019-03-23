fc14a99defacd0860c7d2cc1af6c4aba30839e6a
Kevin Andersson <kevinandersson456@gmail.com>
2019-03-23 00:47:50 -0300

    Agregué la función auxiliar percola, que dice si el sistema percola

--- percolacion.c
+++ percolacion.c
@@ -11,29 +11,43 @@
 float aleatorio(int *seed); 
 int poblar(int *red, int dim, float p);
 int imprimir(int *red, int dim);
-int clasificar(int *red, int dim);
+int clasificar(int *red, int *hist,int dim);
+int hoshen(int *red,int *hist, int s1, int s2, int i);
+int corregir_etiqueta(int *red,int *hist, int dim);
+int percola(int *red, int dim);
+
 
 int main(int argc,char *argv[]){
 	int dim;
 	float p;
 	dim=3;
 	p=0.5;
-	
+		
 	sscanf(argv[1],"%i",&dim);
 	sscanf(argv[2],"%f",&p);	
 
 	int *red;
 	red = (int*)malloc(dim*dim*sizeof(int)); 
-
+	int *hist;
+	hist= (int*)malloc((dim*dim/2+2)*sizeof(int));
+	
+	for(int i=0; i<(dim*dim/2+2); i++){
+		*(hist+i)=i;
+	}
 	
 	//printf("%d %.1f",dim,p);	
 
 	poblar(red,dim,p); //cuando le paso una variable a una funcion no pongo el asterisco a los punteros.
 	imprimir(red,dim);
 	
-	clasificar(red,dim);	
-	printf("\n");
+	clasificar(red,hist,dim);	
+	
 	imprimir(red,dim);
+	corregir_etiqueta(red,hist,dim);
+	
+	imprimir(red,dim);
+	
+	percola(red,dim);
 }
 
 float aleatorio(int *seed){
@@ -68,17 +82,20 @@ int imprimir(int *red, int dim){
 	
 	for (int i=0; i<dim; i++){
 		for (int j=0; j<dim; j++){
-			printf("%d ", *(red+i*dim+j));
+			printf("%d\t", *(red+i*dim+j));
 		}
 		printf("\n");
 	}
+	printf("\n");
 	return 0;
 }
 
-int clasificar(int *red, int dim){
+int clasificar(int *red, int *hist, int dim){
 
 	int label=2;
 	int s1,s2;
+	
+	
 	if(*red==1){	//esto se encarga del primer nodo
 		*red=label;
 		label++;
@@ -124,9 +141,9 @@ int clasificar(int *red, int dim){
 						*(red+i*dim+j)=s2;
 					}
 				}
-				//else{
-				//	hoshen();
-				//}	
+				else{
+					hoshen(red,hist,s1,s2,i*dim+j);
+				}	
 			}
 			
 		}
@@ -137,3 +154,68 @@ int clasificar(int *red, int dim){
 	return 0;
 }
 
+int hoshen(int *red,int *hist, int s1, int s2, int i){
+
+	int max,min;
+	
+	while(*(hist+s1)<0){
+		s1=-*(hist+s1);
+	}
+	while(*(hist+s2)<0){
+		s2=-*(hist+s2);
+	}
+	min=s1;
+	max=s2;
+	if(s2<s1){
+		min=s2;
+		max=s1;
+	}
+	
+	*(red+i)=min;
+	*(hist+max)=-min;
+	*(hist+min)=min;	
+	
+	return 0;
+}
+
+int corregir_etiqueta(int *red,int *hist,int dim){
+	int a;
+	for(int i=0; i<dim*dim; i++ ){
+		a=*(red+i);
+		while(*(hist+a)<0){
+			a=-*(hist+a);
+		}
+		*(red+i)=a;
+	}
+	return 0;
+}
+
+int percola(int *red,int dim){
+	int *perc1;
+	int *perc2;
+	int a,b,percola;
+	percola=0;
+	
+	perc1=(int*)malloc((dim*dim/2+2)*sizeof(int));
+	perc2=(int*)malloc((dim*dim/2+2)*sizeof(int));
+	
+	for(int i=0; i<(dim*dim/2+2); i++){
+		*(perc1+i)=0;
+		*(perc2+i)=0;
+	}
+	for(int i=0; i<dim; i++){
+		a=*(red+i);
+		b=*(red+(dim-1)*dim+i);
+		*(perc1+a)+=a;
+		*(perc2+b)+=b;
+	}	
+	for(int i=2; i<(dim*dim/2+2); i++){
+		if((*(perc1+i))*(*(perc2+i))!=0){
+			percola=1;
+			printf("El sistema percola!\n");
+			
+		}
+	}
+
+	return percola;
+}
